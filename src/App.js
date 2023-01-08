10c1e2a58dd85206142a58f3a113a1241a589602
Vasily <vasily@MacBook-Pro-Vasily.local>
2023-01-09 00:22:50 +0300

    init: start react movies project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Footer } from "./layout/Footer";
+import { Main } from "./layout/Main";
+import { Header } from "./layout/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
